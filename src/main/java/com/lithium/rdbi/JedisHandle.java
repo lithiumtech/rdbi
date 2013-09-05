8fe307739066a01a765d7514ee48385d71281916
Phu Nguyen <phutwo@me.com>
2013-09-04 23:18:26 -0500

    switch from autoclosable of java 7 to closable of java 6

--- src/main/java/com/lithium/rdbi/JedisHandle.java
+++ src/main/java/com/lithium/rdbi/JedisHandle.java
@@ -4,13 +4,15 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.annotation.concurrent.NotThreadSafe;
+import java.io.Closeable;
 
 @NotThreadSafe
-public class JedisHandle implements AutoCloseable {
+public class JedisHandle implements Closeable {
 
     private final JedisPool pool;
     private final Jedis jedis;
     private JedisWrapperDoNotUse jedisWrapper;
+    private boolean closed = false;
 
     private final ProxyFactory proxyFactory;
 
@@ -36,6 +38,10 @@ public class JedisHandle implements AutoCloseable {
     @Override
     public void close() {
 
+        if (closed) {
+            return;
+        }
+
         try {
             if (jedisWrapper.__rdbi_isJedisBusted__()) {
                 pool.returnBrokenResource(jedis);
@@ -43,6 +49,10 @@ public class JedisHandle implements AutoCloseable {
                 pool.returnResource(jedis);
             }
         } catch (ClassCastException e) {
+            //internal error
+        } finally {
+            //for indempotency of Closeable interface
+            closed = true;
         }
     }
 }
