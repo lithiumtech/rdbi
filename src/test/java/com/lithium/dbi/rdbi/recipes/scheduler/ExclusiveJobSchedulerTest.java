842bedbb699c9235bd417254a6fa38ac68c14886
Dave Wingate <Dave.Wingate@lithium.com>
2014-12-05 14:17:46 -0600

    isolate tests into separate tubes

--- src/test/java/com/lithium/dbi/rdbi/recipes/scheduler/ExclusiveJobSchedulerTest.java
+++ src/test/java/com/lithium/dbi/rdbi/recipes/scheduler/ExclusiveJobSchedulerTest.java
@@ -3,6 +3,7 @@ package com.lithium.dbi.rdbi.recipes.scheduler;
 import com.lithium.dbi.rdbi.Callback;
 import com.lithium.dbi.rdbi.Handle;
 import com.lithium.dbi.rdbi.RDBI;
+import com.lithium.dbi.rdbi.testutil.TubeUtils;
 import org.joda.time.Instant;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -21,12 +22,13 @@ import static org.testng.Assert.assertTrue;
 public class ExclusiveJobSchedulerTest {
 
     private static final RDBI rdbi = new RDBI(new JedisPool("localhost"));
-    private static final String TEST_TUBE = "mytube";
+    private String tubeName;
 
     private ExclusiveJobScheduler scheduledJobSystem = null;
 
     @BeforeMethod
     public void setup(){
+        tubeName = TubeUtils.uniqueTubeName();
         scheduledJobSystem  = new ExclusiveJobScheduler(rdbi, "myprefix:");
     }
 
@@ -36,89 +38,89 @@ public class ExclusiveJobSchedulerTest {
         rdbi.withHandle(new Callback<Void>() {
             @Override
             public Void run(Handle handle) {
-                handle.jedis().del(scheduledJobSystem.getReadyQueue(TEST_TUBE), scheduledJobSystem.getRunningQueue(TEST_TUBE));
+                handle.jedis().del(scheduledJobSystem.getReadyQueue(tubeName), scheduledJobSystem.getRunningQueue(tubeName));
                 return null;
             }
         });
 
         // ensure system is not paused
-        scheduledJobSystem.resume(TEST_TUBE);
+        scheduledJobSystem.resume(tubeName);
     }
 
     @Test
     public void testBasicSchedule() throws InterruptedException {
 
-        scheduledJobSystem.schedule(TEST_TUBE, "{hello:world}", 0);
-        JobInfo result2 = scheduledJobSystem.reserveSingle(TEST_TUBE, 1000);
+        scheduledJobSystem.schedule(tubeName, "{hello:world}", 0);
+        JobInfo result2 = scheduledJobSystem.reserveSingle(tubeName, 1000);
         assertEquals(result2.getJobStr(), "{hello:world}");
-        JobInfo result3 = scheduledJobSystem.reserveSingle(TEST_TUBE, 1000);
+        JobInfo result3 = scheduledJobSystem.reserveSingle(tubeName, 1000);
         assertNull(result3);
     }
 
     @Test
     public void testCull() throws InterruptedException {
 
-        scheduledJobSystem.schedule(TEST_TUBE, "{hello:world}", 0);
-        JobInfo result3 = scheduledJobSystem.reserveSingle(TEST_TUBE, 1000);
+        scheduledJobSystem.schedule(tubeName, "{hello:world}", 0);
+        JobInfo result3 = scheduledJobSystem.reserveSingle(tubeName, 1000);
         assertNotNull(result3);
         Thread.sleep(2000);
-        List<TimeJobInfo> infos = scheduledJobSystem.removeExpiredJobs(TEST_TUBE);
+        List<TimeJobInfo> infos = scheduledJobSystem.removeExpiredJobs(tubeName);
         assertEquals(infos.size(), 1);
         assertNotNull(infos.get(0).getJobScore());
     }
 
     @Test
     public void testDelete() throws InterruptedException {
-        scheduledJobSystem.schedule(TEST_TUBE, "{hello:world}", 0);
-        JobInfo result2 = scheduledJobSystem.reserveSingle(TEST_TUBE, 1000);
+        scheduledJobSystem.schedule(tubeName, "{hello:world}", 0);
+        JobInfo result2 = scheduledJobSystem.reserveSingle(tubeName, 1000);
         assertEquals(result2.getJobStr(), "{hello:world}");
 
         //while in the running queue
-        scheduledJobSystem.deleteJob(TEST_TUBE, result2.getJobStr());
-        JobInfo result3 = scheduledJobSystem.reserveSingle(TEST_TUBE, 1000);
+        scheduledJobSystem.deleteJob(tubeName, result2.getJobStr());
+        JobInfo result3 = scheduledJobSystem.reserveSingle(tubeName, 1000);
         assertNull(result3);
 
-        scheduledJobSystem.schedule(TEST_TUBE, "{hello:world}", 1000);
+        scheduledJobSystem.schedule(tubeName, "{hello:world}", 1000);
 
         //while in ready queue
-        scheduledJobSystem.deleteJob(TEST_TUBE, "{hello:world}");
+        scheduledJobSystem.deleteJob(tubeName, "{hello:world}");
 
         Thread.sleep(2000L);
 
-        JobInfo result4 = scheduledJobSystem.reserveSingle(TEST_TUBE, 1000);
+        JobInfo result4 = scheduledJobSystem.reserveSingle(tubeName, 1000);
         assertNull(result4);
     }
 
     @Test
     public void testBasicStates() throws InterruptedException {
-        scheduledJobSystem.schedule(TEST_TUBE, "{hello:world}", 1000);
+        scheduledJobSystem.schedule(tubeName, "{hello:world}", 1000);
 
-        List<TimeJobInfo> jobInfos = scheduledJobSystem.peekDelayed(TEST_TUBE, 0, 1);
+        List<TimeJobInfo> jobInfos = scheduledJobSystem.peekDelayed(tubeName, 0, 1);
         assertEquals(jobInfos.get(0).getJobStr(), "{hello:world}");
 
         Thread.sleep(1500);
-        List<TimeJobInfo> jobInfos2 = scheduledJobSystem.peekDelayed(TEST_TUBE, 0, 1);
+        List<TimeJobInfo> jobInfos2 = scheduledJobSystem.peekDelayed(tubeName, 0, 1);
         assertEquals(jobInfos2.size(), 0);
 
-        List<TimeJobInfo> jobInfos3 = scheduledJobSystem.peekReady(TEST_TUBE, 0, 1);
+        List<TimeJobInfo> jobInfos3 = scheduledJobSystem.peekReady(tubeName, 0, 1);
         assertEquals(jobInfos3.get(0).getJobStr(), "{hello:world}");
 
-        scheduledJobSystem.reserveSingle("mytube", 1000L);
-        List<TimeJobInfo> jobInfos4 = scheduledJobSystem.peekRunning(TEST_TUBE, 0, 1);
+        scheduledJobSystem.reserveSingle(tubeName, 1000L);
+        List<TimeJobInfo> jobInfos4 = scheduledJobSystem.peekRunning(tubeName, 0, 1);
         assertEquals(jobInfos4.get(0).getJobStr(), "{hello:world}");
 
         Thread.sleep(1500);
-        List<TimeJobInfo> jobInfos6 = scheduledJobSystem.peekRunning(TEST_TUBE, 0, 1);
+        List<TimeJobInfo> jobInfos6 = scheduledJobSystem.peekRunning(tubeName, 0, 1);
         assertEquals(jobInfos6.size(), 0);
 
-        List<TimeJobInfo> jobInfos5 = scheduledJobSystem.peekExpired(TEST_TUBE, 0, 1);
+        List<TimeJobInfo> jobInfos5 = scheduledJobSystem.peekExpired(tubeName, 0, 1);
         assertEquals(jobInfos5.get(0).getJobStr(), "{hello:world}");
 
-        scheduledJobSystem.deleteJob(TEST_TUBE,  "{hello:world}");
-        assertEquals(scheduledJobSystem.peekDelayed(TEST_TUBE, 1, 0).size(), 0);
-        assertEquals(scheduledJobSystem.peekReady(TEST_TUBE, 1, 0).size(), 0);
-        assertEquals(scheduledJobSystem.peekRunning(TEST_TUBE, 1, 0).size(), 0);
-        assertEquals(scheduledJobSystem.peekExpired(TEST_TUBE, 1, 0).size(), 0);
+        scheduledJobSystem.deleteJob(tubeName,  "{hello:world}");
+        assertEquals(scheduledJobSystem.peekDelayed(tubeName, 1, 0).size(), 0);
+        assertEquals(scheduledJobSystem.peekReady(tubeName, 1, 0).size(), 0);
+        assertEquals(scheduledJobSystem.peekRunning(tubeName, 1, 0).size(), 0);
+        assertEquals(scheduledJobSystem.peekExpired(tubeName, 1, 0).size(), 0);
     }
 
     @Test
@@ -126,7 +128,7 @@ public class ExclusiveJobSchedulerTest {
 
         Instant before = new Instant();
         for ( int i = 0; i < 10000; i++) {
-            scheduledJobSystem.schedule(TEST_TUBE, "{hello:world} " + i, 0);
+            scheduledJobSystem.schedule(tubeName, "{hello:world} " + i, 0);
         }
         Instant after = new Instant();
         System.out.println("final " + after.minus(before.getMillis()).getMillis());
@@ -135,7 +137,7 @@ public class ExclusiveJobSchedulerTest {
 
         Instant before2 = new Instant();
         for ( int i = 0; i < 10000; i++) {
-            scheduledJobSystem.reserveSingle(TEST_TUBE, 1);
+            scheduledJobSystem.reserveSingle(tubeName, 1);
         }
 
         Instant after2 = new Instant();
@@ -144,38 +146,38 @@ public class ExclusiveJobSchedulerTest {
 
     @Test
     public void testIsPaused(){
-        assertFalse(scheduledJobSystem.isPaused(TEST_TUBE));
-        scheduledJobSystem.pause(TEST_TUBE);
-        assertTrue(scheduledJobSystem.isPaused(TEST_TUBE));
-        scheduledJobSystem.resume(TEST_TUBE);
-        assertFalse(scheduledJobSystem.isPaused(TEST_TUBE));
+        assertFalse(scheduledJobSystem.isPaused(tubeName));
+        scheduledJobSystem.pause(tubeName);
+        assertTrue(scheduledJobSystem.isPaused(tubeName));
+        scheduledJobSystem.resume(tubeName);
+        assertFalse(scheduledJobSystem.isPaused(tubeName));
     }
 
     @Test
     public void testPause(){
-        assertTrue(scheduledJobSystem.schedule(TEST_TUBE, "{hello:delayed}", 10000), "PRE-CONDITION: failed to create 1 delayed job");
+        assertTrue(scheduledJobSystem.schedule(tubeName, "{hello:delayed}", 10000), "PRE-CONDITION: failed to create 1 delayed job");
         // scheduling a "ready" job with a ttl of 0 means that it may come back when we make the call to peekDelayed
         // unless we force at least a millisecond delay.  Rather than sleep the thread, let's just schedule this to run
         // sometime in the past, making it "overdue" and thus ready
-        assertTrue(scheduledJobSystem.schedule(TEST_TUBE, "{hello:ready}", -10000), "PRE-CONDITION: failed to create 1 ready job");
+        assertTrue(scheduledJobSystem.schedule(tubeName, "{hello:ready}", -10000), "PRE-CONDITION: failed to create 1 ready job");
 
 
-        scheduledJobSystem.pause(TEST_TUBE);
+        scheduledJobSystem.pause(tubeName);
 
-        assertFalse(scheduledJobSystem.schedule(TEST_TUBE, "{hello:world}", 0), "Should not be able to schedule anything when the system is paused.");
-        JobInfo result = scheduledJobSystem.reserveSingle(TEST_TUBE, 1000);
+        assertFalse(scheduledJobSystem.schedule(tubeName, "{hello:world}", 0), "Should not be able to schedule anything when the system is paused.");
+        JobInfo result = scheduledJobSystem.reserveSingle(tubeName, 1000);
         assertNull(result, "Should not have been able to reserve a job while the system is paused");
     }
 
     @Test
     public void testResume(){
-        scheduledJobSystem.pause(TEST_TUBE);
-        assertTrue(scheduledJobSystem.isPaused(TEST_TUBE), "PRE-CONDITION: system should be paused.");
+        scheduledJobSystem.pause(tubeName);
+        assertTrue(scheduledJobSystem.isPaused(tubeName), "PRE-CONDITION: system should be paused.");
 
-        scheduledJobSystem.resume(TEST_TUBE);
-        assertTrue(scheduledJobSystem.schedule(TEST_TUBE, "{hello:delayed}", 10000), "Failed to schedule a delayed job after resuming the system.");
-        assertTrue(scheduledJobSystem.schedule(TEST_TUBE, "{hello:ready}", -10000), "Failed to schedule a ready job after resuming the system.");
-        JobInfo result = scheduledJobSystem.reserveSingle(TEST_TUBE, 1000);
+        scheduledJobSystem.resume(tubeName);
+        assertTrue(scheduledJobSystem.schedule(tubeName, "{hello:delayed}", 10000), "Failed to schedule a delayed job after resuming the system.");
+        assertTrue(scheduledJobSystem.schedule(tubeName, "{hello:ready}", -10000), "Failed to schedule a ready job after resuming the system.");
+        JobInfo result = scheduledJobSystem.reserveSingle(tubeName, 1000);
         assertNotNull(result, "Should have been able to reserve a job while the system is paused");
     }
 
