8b7e111285bbc6ffcbc1dadeac240098412578ea
Jesse Hodges <jesse.hodges@lithium.com>
2018-01-10 16:58:56 -0600

    match indentation of other @Query annotations

--- rdbi-recipes/src/main/java/com/lithium/dbi/rdbi/recipes/scheduler/MultiChannelSchedulerDAO.java
+++ rdbi-recipes/src/main/java/com/lithium/dbi/rdbi/recipes/scheduler/MultiChannelSchedulerDAO.java
@@ -117,40 +117,40 @@ public interface MultiChannelSchedulerDAO {
      */
     @Mapper(TimeJobInfoListMapper.class)
     @Query(
-                    "local reserved = {}\n" +
-                    "local isPaused = redis.call('GET', $pausedTube$) \n" +
-                    "if isPaused then\n" +
-                    "    return reserved\n" +
-                    "end\n" +
-                    "local nextLimit = tonumber($limit$)\n" +
-                    "local runningLimit = tonumber($runningLimit$)\n" +
-                    "if runningLimit > 0 then\n" +
-                    "  local nowRunning = redis.call('ZCARD', $runningQueue$)\n" +
-                    "  if nextLimit + nowRunning > runningLimit then\n" +
-                    "    return reserved\n" +
-                    "  end\n" +
-                    "end\n" +
-                    "local reservedIndex = 1\n" +
-                    "local nextOffset = 0\n" +
-                    "while nextLimit > 0 do\n" +
-                    "   local jobs = redis.call('ZRANGEBYSCORE', $readyQueue$, 0, $now$, 'WITHSCORES', 'LIMIT', nextOffset, nextLimit)\n" +
-                    "   if next(jobs) == nil then\n" +
-                    "       return reserved\n" +
-                    "   end\n" +
-                    "   for i=1,#jobs,2 do\n" +
-                    "      local inRunningQueue = redis.call('ZSCORE', $runningQueue$, jobs[i])\n" +
-                    "      if not inRunningQueue then\n" +
-                    "          reserved[reservedIndex] = jobs[i]\n" +
-                    "          reserved[reservedIndex + 1] = jobs[i + 1]\n" +
-                    "          redis.call('ZREM', $readyQueue$, reserved[reservedIndex])\n" +
-                    "          redis.call('ZADD', $runningQueue$, $ttl$, reserved[reservedIndex])\n" +
-                    "          reservedIndex = reservedIndex + 2\n" +
-                    "          nextLimit = nextLimit - 1\n" +
-                    "      end\n" +
-                    "   end\n" +
-                    "   nextOffset = nextOffset + nextLimit\n" +
-                    "end\n" +
-                    "return reserved"
+            "local reserved = {}\n" +
+            "local isPaused = redis.call('GET', $pausedTube$) \n" +
+            "if isPaused then\n" +
+            "    return reserved\n" +
+            "end\n" +
+            "local nextLimit = tonumber($limit$)\n" +
+            "local runningLimit = tonumber($runningLimit$)\n" +
+            "if runningLimit > 0 then\n" +
+            "  local nowRunning = redis.call('ZCARD', $runningQueue$)\n" +
+            "  if nextLimit + nowRunning > runningLimit then\n" +
+            "    return reserved\n" +
+            "  end\n" +
+            "end\n" +
+            "local reservedIndex = 1\n" +
+            "local nextOffset = 0\n" +
+            "while nextLimit > 0 do\n" +
+            "   local jobs = redis.call('ZRANGEBYSCORE', $readyQueue$, 0, $now$, 'WITHSCORES', 'LIMIT', nextOffset, nextLimit)\n" +
+            "   if next(jobs) == nil then\n" +
+            "       return reserved\n" +
+            "   end\n" +
+            "   for i=1,#jobs,2 do\n" +
+            "      local inRunningQueue = redis.call('ZSCORE', $runningQueue$, jobs[i])\n" +
+            "      if not inRunningQueue then\n" +
+            "          reserved[reservedIndex] = jobs[i]\n" +
+            "          reserved[reservedIndex + 1] = jobs[i + 1]\n" +
+            "          redis.call('ZREM', $readyQueue$, reserved[reservedIndex])\n" +
+            "          redis.call('ZADD', $runningQueue$, $ttl$, reserved[reservedIndex])\n" +
+            "          reservedIndex = reservedIndex + 2\n" +
+            "          nextLimit = nextLimit - 1\n" +
+            "      end\n" +
+            "   end\n" +
+            "   nextOffset = nextOffset + nextLimit\n" +
+            "end\n" +
+            "return reserved"
     )
     List<TimeJobInfo> reserveJobsForChannel(
             @BindKey("readyQueue") String readyQueue,
