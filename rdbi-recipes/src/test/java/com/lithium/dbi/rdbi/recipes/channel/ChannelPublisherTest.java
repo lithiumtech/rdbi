3759ba4aff77972fcebb58587b37e887321c9836
Jesse Hodges <jesse.hodges@lithium.com>
2022-12-28 17:16:05 -0600

    CARE-no-cglib: put this back

--- rdbi-recipes/src/test/java/com/lithium/dbi/rdbi/recipes/channel/ChannelPublisherTest.java
+++ rdbi-recipes/src/test/java/com/lithium/dbi/rdbi/recipes/channel/ChannelPublisherTest.java
@@ -53,7 +53,7 @@ public class ChannelPublisherTest {
         thread1.start();
         thread2.start();
 
-        long timeToFinish = 5500;
+        long timeToFinish = 1500;
         thread1.join(timeToFinish);
         thread2.join(timeToFinish);
 
