9401bf040aa0de31fe5e302a46a385c4b71e779a
Jesse Hodges <jesse.hodges@lithium.com>
2022-12-29 11:20:06 -0600

    CARE-no-cglib: make it so we can know how actually bad we are

--- rdbi-recipes/src/test/java/com/lithium/dbi/rdbi/recipes/channel/ChannelPublisherTest.java
+++ rdbi-recipes/src/test/java/com/lithium/dbi/rdbi/recipes/channel/ChannelPublisherTest.java
@@ -8,9 +8,17 @@ import org.testng.annotations.Test;
 import redis.clients.jedis.JedisPool;
 
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertNotNull;
@@ -149,6 +157,31 @@ public class ChannelPublisherTest {
         }
     }
 
+    static void timedThreads(long timeLimit, TimeUnit unit, Runnable... runnables) {
+        long timeOut = timeLimit * 10;
+        long start = System.currentTimeMillis();
+
+
+        List<CompletableFuture<Void>> futures = Arrays.stream(runnables)
+                                                      .map(CompletableFuture::runAsync)
+                                                      .collect(Collectors.toList());
+
+        CompletableFuture<Void> all = CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]));
+
+        try {
+            all.get(timeOut, unit);
+        } catch (TimeoutException e) {
+            fail(String.format("did not complete within time limit %d %s, or timeout %d %s", timeLimit, unit, timeOut, unit));
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        long elapsed = System.currentTimeMillis() - start;
+        if (elapsed > timeLimit) {
+            fail(String.format("Did not finish in time. Expected to finish in %d %s, but finished in %d", timeLimit, unit, elapsed));
+        }
+    }
+
     @Test
     public void testPublishChannelPerformanceTest() throws InterruptedException {
 
@@ -159,80 +192,34 @@ public class ChannelPublisherTest {
         channelPublisher.resetChannels(channel);
 
         final List<String> value = ImmutableList.of("value1");
-        final AtomicBoolean thread1Finished = new AtomicBoolean(false);
-        final AtomicBoolean thread2Finished = new AtomicBoolean(false);
 
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 1000; i++) {
-                    channelPublisher.publish(channel, value);
-
-                    if (Thread.interrupted()) {
-                        return;
-                    }
-                }
-                thread1Finished.set(true);
-            }
-        });
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 1000; i++) {
-                    channelPublisher.publish(channel, value);
-                }
-                thread2Finished.set(true);
-            }
-        });
-
-        thread1.start();
-        thread2.start();
-
-        long timeToFinish = 1500;
-        thread1.join(timeToFinish);
-        thread2.join(timeToFinish);
-
-        if (!thread1Finished.get() || !thread2Finished.get()) {
-            fail("Did not finish in time");
-        }
+        timedThreads(1500, TimeUnit.MILLISECONDS,
+                     () -> {
+                         for (int i = 0; i < 1000; i++) {
+                             channelPublisher.publish(channel, value);
+                         }
+                     },
+                     () -> {
+                         for (int i = 0; i < 1000; i++) {
+                             channelPublisher.publish(channel, value);
+                         }
+                     }
+                    );
 
         final ChannelReceiver receiver = new ChannelLuaReceiver(rdbi);
-        final AtomicBoolean thread3Finished = new AtomicBoolean(false);
-        final AtomicBoolean thread4Finished = new AtomicBoolean(false);
 
+        timedThreads(1500, TimeUnit.MILLISECONDS,
+                     () -> {
+                         for (int i = 0; i < 100; i++) {
+                             receiver.get(channel.iterator().next(), 900L);
+                         }
+                     },
+                     () -> {
+                         for (int i = 0; i < 100; i++) {
+                             receiver.get(channel.iterator().next(), 900L);
+                         }
+                     }
+                    );
 
-        Thread thread3 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 100; i++) {
-                    receiver.get(channel.iterator().next(), 900L);
-                }
-                thread3Finished.set(true);
-            }
-        });
-
-        Thread thread4 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 100; i++) {
-                    receiver.get(channel.iterator().next(), 900L);
-                }
-                thread4Finished.set(true);
-            }
-        });
-
-        Instant before = Instant.now();
-        thread3.start();
-        thread4.start();
-
-        thread3.join(timeToFinish);
-        thread4.join(timeToFinish);
-        Instant after = Instant.now();
-
-        if (!thread3Finished.get() || !thread3Finished.get()) {
-            fail("Did not finish in time");
-        }
-
-        System.out.println("final time " + (after.toEpochMilli() - before.toEpochMilli()));
     }
 }
